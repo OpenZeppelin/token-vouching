ce6276a43f5febaa4f7010d8d1681426f36a5790
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-01-25 16:44:10 -0300

    Add testing script using ganache

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,40 @@
+#!/usr/bin/env bash
+
 # Exit script as soon as a command fails.
 set -o errexit
 
-if [ "$SOLIDITY_COVERAGE" = true ]; then
-  echo "Measuring coverage..."
-  node_modules/.bin/solidity-coverage
-  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
-    cat coverage/lcov.info | node_modules/.bin/coveralls
+# Executes cleanup function at script exit.
+trap cleanup EXIT
+
+cleanup() {
+  # Kill ganache instance if it's still running
+  if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
+    kill -9 $ganache_pid
   fi
-else
-  node_modules/.bin/truffle compile
-  node_modules/.bin/truffle test "$@"
-fi
+}
+
+start_ganache() {
+  echo "Starting ganache-cli..."
+  nohup npx ganache-cli -e 10000 -p 8545 > /dev/null &
+  ganache_pid=$!
+  sleep 3
+  echo "Running ganache-cli with pid ${ganache_pid}"
+}
+
+run_tests() {
+  if [ "$SOLIDITY_COVERAGE" = true ]; then
+    echo "Measuring coverage..."
+    node_modules/.bin/solidity-coverage
+    if [ "$CONTINUOUS_INTEGRATION" = true ]; then
+      cat coverage/lcov.info | node_modules/.bin/coveralls
+    fi
+  else
+    echo "Compiling..."
+    truffle compile
+    echo "Running tests..."
+    truffle test --network local "$@"
+  fi
+}
+
+start_ganache
+run_tests
